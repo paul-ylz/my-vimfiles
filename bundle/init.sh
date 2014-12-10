d33aaafc6ffb99acf87f6aef31aef88e80c11c8f
Paul Yeoh <paul@buuuk.com>
2014-12-10 11:09:37 +0800

    init file for cloning plugins

---
+++ bundle/init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Vim Snipmate dependencies
+git clone git@github.com:tomtom/tlib_vim.git ./tlib_vim
+git clone git@github.com:MarcWeber/vim-addon-mw-utils.git ./vim-addon-mw-utils
+# Addons
+git clone git@github.com:garbas/vim-snipmate.git ./vim-snipmate
+git clone git@github.com:honza/vim-snippets.git ./vim-snippets
+git clone git://github.com/godlygeek/tabular.git ./tabular
+git clone git@github.com:tpope/vim-surround.git ./vim-surround
