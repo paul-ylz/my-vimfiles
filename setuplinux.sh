4b484daa06904302054be3050682396cf0da841e
Paul Yeoh <paulyeoh.lz@gmail.com>
2017-02-20 10:38:22 +0800

    remove non vim things

--- setuplinux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd bundle
-./init.sh
-cd ..
-ln -s `pwd`/linux.tmux.conf ../.tmux.conf
-ln -s `pwd`/vimrc ../.vimrc
-echo "source ~/.vim/add_to_profile.txt" >> ../.bashrc
-cd ~
-ls -alF
